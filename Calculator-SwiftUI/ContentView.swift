6635229dac885d28500897063018343d8c3812f3
Sa3doola <saadsherif02@gmail.com>
2021-03-07 10:28:51 +0200

    Initial Commit

---
+++ Calculator-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator-SwiftUI
+//
+//  Created by Saad Sherif on 3/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
